21055bbbab5e45aff0ae0587b6d9f51fcdeb00a1
safumk99 <safumk99@gmail.com>
2026-08-01 11:54:26 +0530

    .env removed

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 from routes.home import home_bp
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 app.register_blueprint(home_bp)
 
---
+++ database.py
@@ -0,0 +1,6 @@
+import psycopg2
+from config import Config
+
+
+def get_connection():
+    return psycopg2.connect(Config.DATABASE_URL)
